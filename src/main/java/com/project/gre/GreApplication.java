d58c222a1d05f533f9a6cea8b94b9ca0e8dd2972
abdullahalnoman <abdullah.alnoman@akelius.de>
2021-02-28 01:57:03 +0100

    Initial commit

---
+++ src/main/java/com/project/gre/GreApplication.java
@@ -0,0 +1,13 @@
+package com.project.gre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/gre/GreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.gre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
