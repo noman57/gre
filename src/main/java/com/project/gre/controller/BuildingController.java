61e7394d4a148c51398c524fa5366a92b55034b9
abdullahalnoman <abdullah.alnoman@akelius.de>
2021-03-01 10:38:42 +0100

    added building controller and test

---
+++ src/main/java/com/project/gre/controller/BuildingController.java
@@ -0,0 +1,64 @@
+package com.project.gre.controller;
+
+import com.project.gre.model.Building;
+import com.project.gre.model.dto.BuildingDTO;
+import com.project.gre.service.BuildingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+
+@RestController
+@RequestMapping("/v1/buildings")
+@CrossOrigin
+public class BuildingController {
+
+    private BuildingService buildingService;
+
+
+    @Autowired
+    public BuildingController(BuildingService buildingService) {
+        this.buildingService = buildingService;
+    }
+
+    @GetMapping("/")
+    public HttpEntity<Page<Building>> getBuildings(final Pageable pageable) {
+        Page<Building> buildingPage = buildingService.findAll(pageable);
+        return ResponseEntity.ok(buildingPage);
+    }
+
+    @GetMapping("/{buildingId}")
+    public ResponseEntity<Building> getBuildingById(@PathVariable Long buildingId) {
+        Building building = buildingService.find(buildingId);
+        return ResponseEntity.ok(building);
+    }
+
+    @DeleteMapping("/{buildingId}")
+    public void deleteBuilding(@PathVariable long buildingId) {
+        buildingService.delete(buildingId);
+    }
+
+    @PostMapping("/")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<?> createBuilding(@Valid @RequestBody BuildingDTO buildingDTO,final UriComponentsBuilder ucBuilder)  {
+        Building building = buildingService.create(buildingDTO);
+        final URI uri = ucBuilder.path("/buildings/{id}").buildAndExpand(building.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @PatchMapping("/{buildingId}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<String> updateBuilding(@PathVariable long buildingId, @Valid @RequestBody BuildingDTO buildingDTO)  {
+        buildingService.update(buildingId,buildingDTO);
+        return ResponseEntity.ok().body("");
+    }
+
+}
+
---
+++ src/test/java/com/project/gre/controller/BuildingControllerTest.java
@@ -0,0 +1,102 @@
+package com.project.gre.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.project.gre.exception.ResourceNotFoundException;
+import com.project.gre.model.Building;
+import com.project.gre.model.dto.BuildingDTO;
+import com.project.gre.service.BuildingService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = BuildingController.class)
+public class BuildingControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private BuildingService buildingService;
+
+
+
+    @Test
+    public void findAllShouldReturnOK() throws Exception {
+        when(buildingService.findAll(any())).thenReturn(Page.empty());
+        mockMvc.perform(MockMvcRequestBuilders.get("/v1/buildings/"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void findByIdShouldReturnBuilding() throws Exception {
+        Building buildingA = new Building();
+        buildingA.setId(1);
+        buildingA.setName("name");
+        when(buildingService.find(any())).thenReturn(buildingA);
+        mockMvc.perform(MockMvcRequestBuilders.get("/v1/buildings/1"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void findByIdShouldThrowException() throws Exception {
+        when(buildingService.find(any())).thenThrow(new ResourceNotFoundException(""));
+        mockMvc.perform(MockMvcRequestBuilders.get("/v1/buildings/1"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void createShouldReturnCreated() throws Exception {
+        Building buildingA = new Building();
+        buildingA.setId(1);
+        buildingA.setName("name");
+        BuildingDTO buildingDTO = new BuildingDTO();
+        buildingDTO.setName("name");
+        when(buildingService.create(any())).thenReturn(buildingA);
+        mockMvc.perform(MockMvcRequestBuilders.post("/v1/buildings/")
+                .contentType(MediaType.APPLICATION_JSON).content(new ObjectMapper().writeValueAsString(buildingDTO)
+                ))
+                .andExpect(status().isCreated());
+    }
+
+
+    @Test
+    public void updateShouldReturnOK() throws Exception {
+        Building buildingA = new Building();
+        buildingA.setId(1);
+        buildingA.setName("name");
+        BuildingDTO buildingDTO = new BuildingDTO();
+        buildingDTO.setName("name");
+        when(buildingService.update(anyLong(),any(BuildingDTO.class))).thenReturn(buildingA);
+        mockMvc.perform(MockMvcRequestBuilders.patch("/v1/buildings/1")
+                .contentType(MediaType.APPLICATION_JSON).content(new ObjectMapper().writeValueAsString(buildingDTO))
+        ).andExpect(status().isOk());
+    }
+
+    @Test
+    public void updateShouldReturnThrowClientException() throws Exception {
+        BuildingDTO buildingDTO = new BuildingDTO();
+        mockMvc.perform(MockMvcRequestBuilders.patch("/v1/buildings/1")
+                .contentType(MediaType.APPLICATION_JSON).content(new ObjectMapper().writeValueAsString(buildingDTO))
+        ).andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void deleteShouldReturnOk() throws Exception {
+        doNothing().when(buildingService).delete(anyLong());
+        mockMvc.perform(MockMvcRequestBuilders.delete("/v1/buildings/1"))
+                .andExpect(status().isOk());
+    }
+}
