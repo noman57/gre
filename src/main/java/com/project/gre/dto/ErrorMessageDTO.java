044610b60608fceb8ef0a0394a594ea848f2093e
abdullahalnoman <abdullah.alnoman@akelius.de>
2021-03-01 21:34:23 +0100

    added global error handler for better error messages

---
+++ src/main/java/com/project/gre/dto/ErrorMessageDTO.java
@@ -0,0 +1,25 @@
+package com.project.gre.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class ErrorMessageDTO {
+
+    private HttpStatus status;
+    private List errors;
+
+
+    public ErrorMessageDTO(final HttpStatus status, final String message) {
+        this.status = status;
+        errors = Arrays.asList(message);
+    }
+}
---
+++ src/main/java/com/project/gre/exception/GlobalErrorHandler.java
@@ -0,0 +1,52 @@
+package com.project.gre.exception;
+
+import com.project.gre.dto.ErrorMessageDTO;
+import javassist.tools.web.BadHttpRequest;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class GlobalErrorHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler({ResourceNotFoundException.class})
+    public ResponseEntity<ErrorMessageDTO> handleResourceNotFound(final ResourceNotFoundException ex) {
+        final ErrorMessageDTO apiError = new ErrorMessageDTO(HttpStatus.NOT_FOUND, ex.getMessage());
+        return new ResponseEntity<>(apiError, new HttpHeaders(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler({BadHttpRequest.class})
+    public ResponseEntity<ErrorMessageDTO> handelBadRequestException(final BadHttpRequest ex) {
+        final ErrorMessageDTO apiError = new ErrorMessageDTO(HttpStatus.BAD_REQUEST, ex.getMessage());
+        return new ResponseEntity<>(apiError, new HttpHeaders(), HttpStatus.BAD_REQUEST);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException exception,
+            HttpHeaders headers,
+            HttpStatus status, WebRequest request) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", new Date());
+        body.put("status", status.value());
+        List<String> errors = exception.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.toList());
+        body.put("errors", errors);
+        return new ResponseEntity<>(body, headers, status);
+    }
+}
